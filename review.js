6837b3908c323281f13cabd04ae3033e3c5784fb
soko <soko2028@tisbrain.net>
2025-05-13 08:41:35 +0900

    Create review.js

---
+++ review.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('.home-button').addEventListener('click', function() {
+        alert('Home button clicked!');
+    });
+    document.querySelector('.review-input').addEventListener('click', function() {
+        alert('Review added!');
+    });
+});
