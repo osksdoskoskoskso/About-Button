81f1cab3c8922ae1f4c57966636483e5b2d875f6
soko <soko2028@tisbrain.net>
2025-05-13 08:15:25 +0900

    Create checkout.js

---
+++ checkout.js
@@ -0,0 +1,9 @@
+document.querySelector('.pay-button').addEventListener('click', function() {
+    alert('Processing payment. Thank you!');
+});
+document.querySelector('.order-button').addEventListener('click', function() {
+    alert('Order confirmed. Thank you for your purchase!');
+});
+document.querySelector('.save-button').addEventListener('click', function() {
+    alert('Saved details thank you!');
+});
