01ebef4a422579596cd9a61bb07d8f3adbfb8adc
osksdoskoskoskso <soko2028@tisbrain.net>
2025-05-12 13:47:48 +0900

    Create AboutUs.js

---
+++ AboutUs.js
@@ -0,0 +1,7 @@
+function aboutUs() {
+  alert("Our name is Soichiro and Dinula. We are selling a product called the about button, when we press it we get a brief explanation about the person");
+}
+
+function ownerInfo() {
+  alert("Soichiro is 14 years old and Dinula is 15 years old. We are both one of the smartest student from stanford university");
+}
