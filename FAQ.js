ceda814090838d73c8ce17814463d324d743e4aa
soko <soko2028@tisbrain.net>
2025-05-15 10:23:52 +0900

    Create FAQ.js

---
+++ FAQ.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('.home-button').addEventListener('click', function() {
+        alert('Redirecting to home!');
+    });
